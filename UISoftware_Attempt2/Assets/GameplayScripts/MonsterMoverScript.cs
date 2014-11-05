00a66f3f65639380cd974f7acfe7debd0bf87360
Arindam Bose <arindam.b@gatech.edu>
2014-11-04 21:07:05 -0500

    Bug Fixes
    +Fixed a bug with monster and wall collision

--- UISoftware_Attempt2/Assets/GameplayScripts/MonsterMoverScript.cs
+++ UISoftware_Attempt2/Assets/GameplayScripts/MonsterMoverScript.cs
@@ -17,11 +17,13 @@ public class MonsterMoverScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = transform.position + transform.forward * moveStep * Time.deltaTime;
+		if (GameObject.Find ("resourceMine").GetComponent<DefaultTrackableEventHandler> ().Tracked) {
+						transform.position = transform.position + transform.forward * moveStep * Time.deltaTime;
+				}
 	}
 
 	void OnTriggerEnter(Collider hit){
-		if (hit.gameObject.name == "teapot") {
+		if (hit.gameObject.name == targetName) {
 			Debug.Log("HIT BASE");
 			hit.GetComponent<HealthManagerScript>().damage(5);
 			transform.FindChild("monsterModel").particleSystem.Play();
